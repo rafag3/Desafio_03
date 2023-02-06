6385beb437698bd851022d7b06ac8c7a70a69bfd
rafag3 <rafag3.batera@gmail.com>
2023-02-06 10:18:17 -0300

    desafio realizado com excelencia!

---
+++ Desafio_03/Desafio_03/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Quantos alunos você deseja adicionar?");
+        int quantidadeAlunos = int.Parse(Console.ReadLine());
+
+        string nomeAlunoComMaiorNota = "";
+        float maiorNota = 0.0f;
+
+        for (int i = 0; i < quantidadeAlunos; i++)
+        {
+            Console.WriteLine("Digite o nome do aluno:");
+            string nome = Console.ReadLine();
+
+            Console.WriteLine("Digite a nota do aluno:");
+            float nota = float.Parse(Console.ReadLine());
+
+            if (nota > maiorNota)
+            {
+                maiorNota = nota;
+                nomeAlunoComMaiorNota = nome;
+            }
+        }
+
+        Console.WriteLine("O aluno com a maior nota é: " + nomeAlunoComMaiorNota + " com nota " + maiorNota);
+        Console.ReadLine();
+    }
+}
